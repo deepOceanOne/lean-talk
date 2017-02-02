8923a5825f5f986a37332527d169497825428331
lifeinbofyx@sina.com <lifeinbofyx@sina.com>
2017-02-02 19:33:07 +0800

    adapt commit
    modify index.js to get adapted.

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
-'use strict';
 
-var AV = require('leanengine');
-
-var APP_ID = process.env.LC_APP_ID;
-var APP_KEY = process.env.LC_APP_KEY;
-var MASTER_KEY = process.env.LC_APP_MASTER_KEY;
-
-AV.initialize(APP_ID, APP_KEY, MASTER_KEY);
 
 const role=require('role-model');
 const app=role.createRobotApp({chatService:'bearychat'});
