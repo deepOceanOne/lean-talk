7069a5abad7bded7745d8a2203f1a9e912985afc
lifeinbofyx@sina.com <lifeinbofyx@sina.com>
2017-02-12 14:44:56 +0800

    first update
    change to Yixin project

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-'use strict';
-
-
-const role=require('role-model');
-const app=role.createRobotApp({chatService:'bearychat'});
-
-app.run();
+var Yixin = require('./');
+var yixin = new Yixin('thisisonesimpletoken');
+app.configure(function() {
+  app.use(express.query());
+  app.use('/yinxin', yixin.middleware);
+  yixin
+    .text(function(req, res) {
+      var message = req.yixin; //用户的消息通过`req.yixin`获取 
+      res.reply('hello world');
+    })
+    .image(function(req, res) {
+      //... 
+    });
+  //... 
+});
+ 
